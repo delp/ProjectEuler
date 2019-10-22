4e1dceb39725e55e7776635d102cf8d5687a17d7
scud <scud@thergothon>
2019-10-22 15:30:42 -0700

    Add comment on how to make problem 3 faster

--- p003.c
+++ p003.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include "utils.h"
 
+//THE WAY TO MAKE THIS FASTER IS TO JUST IMAGINE MAKING THE NUMBER SMALLER
+//AS YOU TRY FACTORS, LIKE THIS
+//        120
+//      /     \
+//     2      60
+//           /  \ 
+//         6    10
+//       /  \  /   \
+//      2   3 2     5
+
 int main() {
 
 	long num = 600851475143;
-	
+
 	long index;
 	for(index = 1; index < num; index++) {
 		if(isFactorLong(index, num)) {
